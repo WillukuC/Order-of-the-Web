92e8e091bb0bcd878e9ee925a679918e947fbc6e
WillukuC <williamnclark5@gmail.com>
2025-02-21 23:09:15 -0500

    forgor to add Bootstrap script lmfao

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Navbar } from 'react-bootstrap'
-import NavBarBrand from './NavBarBrand'
-import UserProfilePage from '../pages/UserProfilePage'
+// import { Link } from 'react-router-dom'
+// import NavBarBrand from './NavBarBrand'
+// import UserProfilePage from '../pages/UserProfilePage'
 
 export const NavBar = () => {
-  return (
-    <nav className='navbar navbar-expand-lg bg-light'>
-        <div className="container-fluid">
-            <NavBarBrand/>
-            <button className="navbar-toggler" type='button' data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label='Toggle navigation'>
-                <span className="navbar-toggler-icon"/>
-            </button>
-            <div className="collapse navbar-collapse" id='navbarSupportedContent'>
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <Link className="nav-item" to={UserProfilePage}></Link>
-                    <li className="nav-item"></li>
-                    <li className="nav-item"></li>
-                </ul>
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuItems" aria-controls="menuItems" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="menuItems">
+                    <a className="navbar-brand" href="#">Hidden brand</a>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
